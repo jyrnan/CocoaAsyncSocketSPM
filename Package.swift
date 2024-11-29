79c07ae3e0be6e522f1c2482eeecc48074a56301
jyrnan <yoeking@gmail.com>
2024-11-29 16:39:16 +0800

    init

--- Package.swift
+++ Package.swift
@@ -14,8 +14,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CocoaAsyncSocketSPM"),
+        .binaryTarget(
+            name: "CocoaAsyncSocketSPM",
+            url: "https://github.com/jyrnan/CocoaAsyncSocketSPM/releases/download/1.0.0/CocoaAsyncSocket.xcframework.zip",
+            checksum: "28e043d33bde7420c3a58ce38b6594304614913c9ba7ab054f5eec1c4f3801bf"),
 
     ]
 )
